2a3526209350671e2bb34953e8a62747a6e0ecd9
PeterKellett <peterwkellett@gmail.com>
2020-07-21 15:59:41 +0000

    CRUD operations deployed for player names

--- run.py
+++ run.py
@@ -18,12 +18,68 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html", stats=mongo.db.match_stats.find())
+    return render_template("index.html",
+                           stats=mongo.db.match_stats.find())
+
+
+@app.route("/get_data")
+def get_data():
+    stats = mongo.db.players.find()
+    json_data = []
+    for stat in stats:
+        json_data.append(stat)
+    return json_data
 
 
 @app.route("/update_data")
 def update_data():
-    return render_template("update-data.html")
+    return render_template("update-data.html",
+                           players=mongo.db.players.find())
+
+
+@app.route('/manage_players')
+def manage_players():
+    return render_template('players.html',
+                           players=list(mongo.db.players.find()))
+
+
+@app.route("/add_player")
+def add_player():
+    return render_template('add-player.html')
+
+
+@app.route("/insert_player", methods=['POST'])
+def insert_player():
+    players = mongo.db.players
+    player_doc = {'player_name': request.form.get('player_name')}
+    players.insert_one(player_doc)
+    return redirect(url_for('update_data'))
+
+
+@app.route("/edit_player/<player_id>")
+def edit_player(player_id):
+    return render_template('edit-player.html', player=mongo.db.players.find_one({'_id': ObjectId(player_id)}))
+
+
+@app.route("/update_player/<player_id>", methods=["POST"])
+def update_player(player_id):
+    mongo.db.players.update(
+        {'_id': ObjectId(player_id)},
+        {'player_name': request.form.get('player_name')})
+    return redirect(url_for('manage_players'))
+
+
+@app.route("/delete_player/<player_id>")
+def delete_player(player_id):
+    mongo.db.players.remove({'_id': ObjectId(player_id)})
+    return redirect(url_for('manage_players'))
+
+
+@app.route("/insert_data", methods=['POST'])
+def insert_data():
+    stats = mongo.db.match_stats
+    stats.insert(request.form.to_dict())
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
